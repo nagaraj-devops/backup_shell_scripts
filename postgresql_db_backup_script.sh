83245b43f9f9b591c38f503760ef7e6ce29d9137
nagaraj-devops <147501947+nagaraj-devops@users.noreply.github.com>
2024-07-22 13:53:58 +0530

    Update postgresql_db_backup_script.sh
    fixes in shell script for relative path

--- postgresql_db_backup_script.sh
+++ postgresql_db_backup_script.sh
@@ -11,5 +11,5 @@ PROJECT_NAME=${PROJECT_NAME}
 TYPE="prod"
 BACKUP_FILE_NAME="${PROJECT_NAME}_${TYPE}_db_${TODAY_DATE}.pgsql"
 
-sudo PGPASSWORD=${PG_DB_PASSWORD} pg_dump -p 5432 -h ${PG_DB_HOST} -U ${PG_DB_USER} ${PG_DB_NAME} > /$dldir/${BACKUP_FILE_NAME}
-aws s3 mv /home/$USER/$dldir/${BACKUP_FILE_NAME} s3://${S3_BUCKET_NAME}
+sudo PGPASSWORD=${PG_DB_PASSWORD} pg_dump -p 5432 -h ${PG_DB_HOST} -U ${PG_DB_USER} ${PG_DB_NAME} > $dldir/${BACKUP_FILE_NAME}
+aws s3 mv $dldir/${BACKUP_FILE_NAME} s3://${S3_BUCKET_NAME}
