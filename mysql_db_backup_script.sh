88d7363fd14ed015911d34c1d4d9288d4d6c1724
nagaraj-devops <147501947+nagaraj-devops@users.noreply.github.com>
2024-07-22 14:41:56 +0530

    Update mysql_db_backup_script.sh
    path of shell file changed

--- mysql_db_backup_script.sh
+++ mysql_db_backup_script.sh
@@ -11,6 +11,6 @@ PROJECT_NAME=${PROJECT_NAME}
 TYPE="prod"
 BACKUP_FILE_NAME="${PROJECT_NAME}_${TYPE}_db_${TODAY_DATE}.sql"
 
-sudo mysqldump -h ${DB_HOST} -u${DB_USER} -p${DB_PASSWORD} --set-gtid-purged=OFF --column-statistics=0 ${DB_NAME} | gzip -c > /$dldir/${BACKUP_FILE_NAME}
+sudo mysqldump -h ${DB_HOST} -u${DB_USER} -p${DB_PASSWORD} --set-gtid-purged=OFF --column-statistics=0 ${DB_NAME} | gzip -c > $dldir/${BACKUP_FILE_NAME}
 #mysqldump -h ${DB_HOST} -u ${DB_USER} -p${DB_PASS} ${DB_NAME} > /home/$USER/${BACKUP_FILE_NAME}
-aws s3 mv /home/$USER/$dldir/${BACKUP_FILE_NAME} s3://${S3_BUCKET_NAME}
+aws s3 mv $dldir/${BACKUP_FILE_NAME} s3://${S3_BUCKET_NAME}
